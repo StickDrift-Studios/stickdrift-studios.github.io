8de92c5da7b3dc29b8f07eef938cfde4157cff11
AngeDragon <angedragon7@gmail.com>
2026-05-17 16:20:25 -0400

    Add files via upload

--- Idle Game Project/main.js
+++ Idle Game Project/main.js
@@ -1,11 +1,26 @@
 var Tests = 0;
+var Testers = 0;
+
+function save(){ //save the game
+	var save = {
+	tests: Tests,
+	testers: Testers
+};
+	localStorage.setItem("save",JSON.stringify(save));
+};
+function load(){ //load the game
+	var savegame = JSON.parse(localStorage.getItem("save"));
+	if(typeof savegame.tests !== "undefined") Tests = savegame.tests; //load Tests
+	if(typeof savegame.testers !== "undefined") Testers = savegame.testers;   //load Testers
+};
+function deleteSave(){ //Deletes the Save File
+	localStorage.removeItem("save");
+};
 
 function testClick(number){ //Increments Test amount
 	Tests = Tests + number;
-	document.getElementById("tests").innerHTML = Tests; //shows tests amount
+	document.getElementById("tests").innerHTML = prettify(Tests);//shows tests amount
 };
-
-var Testers = 0;
 
 function buyTester(){
 	var testerCost = Math.floor(10 * Math.pow(1.1,Testers)); //Checks the price of this tester
@@ -24,3 +39,9 @@ window.setInterval(function(){ //repeats everything every 1s
 	testClick(Testers);
 	
 }, 1000); //1000 = 1000 ms = 1s
+
+function prettify(input){
+	var output = Math.round(input);
+	return output;
+};
+
